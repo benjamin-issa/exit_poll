8422c4f15bb32774a2643f5127ec5d810508648e
bfissa <benjaminissa@Pineapple.local>
2016-09-19 08:48:05 -0600

    Well, it's compiling now, so....

--- exit_poll_site/voters/views.py
+++ exit_poll_site/voters/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
 
+from .models import Voter
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the voters index.")
+    voter_list = Voter.objects.order_by('-respondent_id')[:30000]
+    output = ', '.join([v.name for v in voter_list])
+    template = loader.get_template('voters/index.html')
+    context = {
+        'voter_list': voter_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, respondent_id):
+    try:
+        voter = Voter.objects.get(pk=respondent_id)
+    except Voter.DoesNotExist:
+        raise Http404("No respondent with that respondent id was found.")
+    return render(request, 'voters/detail.html', {'voter': voter})
