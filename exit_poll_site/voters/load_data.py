1ce821a4d922e8f5e32654a954e750e68cefc6ab
rehtaehb <rehtaehbeers@gmail.com>
2016-10-04 22:16:16 -0600

    Update load_data.py

--- exit_poll_site/voters/load_data.py
+++ exit_poll_site/voters/load_data.py
@@ -14,14 +14,15 @@ dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
 # or import csv
 # dataReader = csv.reader(codecs.open(csv_filepathname, ‘rU’, ‘utf-16’))
 for row in dataReader:
-if row[0] != 'Respondent ID': # Ignore the header row, import everything else
+if row[0] != 'Name': # Ignore the header row, import everything else
 vot = Voter()
-vot.respondent_id = row[0]
-vot.respondent_age = row[1]
-vot.respondent_wave = row[2]
-vot.spanish_speaking = row[3]
-vot.done_online_survey = row[4]
-vot.phone_no_one = row[5]
-vot.phone_no_two = row[6]
-vot.phone_no_three = row[7]
+vot.name = row[0]
+vot.respondent_id = row[1]
+vot.respondent_age = row[2]
+vot.respondent_wave = row[3]
+vot.spanish_speaking = row[4]
+vot.done_online_survey = row[5]
+vot.phone_no_one = row[6]
+vot.phone_no_two = row[7]
+vot.phone_no_three = row[8]
 vot.save()
