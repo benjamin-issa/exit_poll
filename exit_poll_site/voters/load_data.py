ff7ee9daf99c63c3749fc8300ccfd30f8b55a320
rehtaehb <rehtaehbeers@gmail.com>
2016-10-04 21:37:31 -0600

    Update load_data.py

--- exit_poll_site/voters/load_data.py
+++ exit_poll_site/voters/load_data.py
@@ -18,9 +18,10 @@ if row[0] != 'ZIPCODE': # Ignore the header row, import everything else
 vot = Voter()
 vot.respondent_id = row[0]
 vot.respondent_age = row[1]
-vot.spanish_speaking = row[2]
-vot.done_online_survey = row[3]
-vot. phone_no_one = row[4]
-vot. phone_no_two = row[5]
-vot. phone_no_three = row[6]
+vot.respondent_wave = row[2]
+vot.spanish_speaking = row[3]
+vot.done_online_survey = row[4]
+vot. phone_no_one = row[5]
+vot. phone_no_two = row[6]
+vot. phone_no_three = row[7]
 vot.save()
