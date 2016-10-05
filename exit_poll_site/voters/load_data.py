81332573025e74aed0142cbed8ee15c01078d1f7
rehtaehb <rehtaehbeers@gmail.com>
2016-10-04 21:31:17 -0600

    Create load_data.py
    This should upload files

---
+++ exit_poll_site/voters/load_data.py
@@ -0,0 +1,26 @@
+# Full path and name to your csv file
+csv_filepathname="..."
+# Full path to your django project directory
+project_home="..."
+
+import sys,os
+sys.path.append(project_home)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+from voter.models import Voter
+
+import csv
+dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
+# or import csv
+# dataReader = csv.reader(codecs.open(csv_filepathname, ‘rU’, ‘utf-16’))
+for row in dataReader:
+if row[0] != 'ZIPCODE': # Ignore the header row, import everything else
+vot = Voter()
+vot.respondent_id = row[0]
+vot.respondent_age = row[1]
+vot.spanish_speaking = row[2]
+vot.done_online_survey = row[3]
+vot. phone_no_one = row[4]
+vot. phone_no_two = row[5]
+vot. phone_no_three = row[6]
+vot.save()
