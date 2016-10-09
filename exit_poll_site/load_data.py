95b529d3810ddb88efde049b220d67f14f0592f6
rehtaehb <rehtaehbeers@gmail.com>
2016-10-08 21:36:52 -0600

    Update load_data.py

--- exit_poll_site/load_data.py
+++ exit_poll_site/load_data.py
@@ -2,6 +2,7 @@ import csv
 from voters.models import Voter
 from voters.models import Phone
 i=0
+blank=""
 with open('C:\Users\Beers\Downloads\call_list_test.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
@@ -23,9 +24,12 @@ with open('C:\Users\Beers\Downloads\call_list_test.csv', 'rb') as f:
       s_speaking= False
       done_survey = False
       fname= row[1] + " " + row[3]
-      blank=""
+      
       ph_num1=row[7]
       val1=True
+      if ph_num1==" ":
+        val1=False
+        ph_num1=blank
       if ph_num1==".":
         val1=False
         ph_num1=blank
@@ -38,6 +42,9 @@ with open('C:\Users\Beers\Downloads\call_list_test.csv', 'rb') as f:
 
       ph_num2=row[8]
       val2=True
+      if ph_num2==" ":
+        val2=False
+        ph_num2=blank
       if ph_num2==".":
         val2=False
         ph_num2= blank
@@ -50,6 +57,9 @@ with open('C:\Users\Beers\Downloads\call_list_test.csv', 'rb') as f:
       
       ph_num3=row[9]
       val3=True
+      if ph_num3==" ":
+        val3=False
+        ph_num3=blank
       if ph_num3==".":
         val3=False
         ph_num3=blank
@@ -63,8 +73,39 @@ with open('C:\Users\Beers\Downloads\call_list_test.csv', 'rb') as f:
       num1=Phone(phone_no=ph_num1, valid=val1)
       num2=Phone(phone_no=ph_num2, valid=val2)
       num3=Phone(phone_no=ph_num3, valid=val3)
-      num1.save()
-      num2.save()
-      num3.save()
-      res = Voter(name=fname,respondent_id =r_id,respondent_age =age, spanish_speaking =s_speaking, respondent_wave = r_wave, done_online_survey=done_survey,  phone_no_one =num1,  phone_no_two =num2,  phone_no_three =num3)
+
+## has all three numbers
+      if val1 == True & val2 == True & val3 == True:
+        num1.save()
+        num2.save()
+        num3.save()
+        res = Voter(name=fname,respondent_id =r_id,respondent_age =age, spanish_speaking =s_speaking, respondent_wave = r_wave, done_online_survey=done_survey,  phone_no_one =num1,  phone_no_two =num2,  phone_no_three =num3)
+## has phone number 1 only
+      if val1 == True & val2 == False & val3 == False:
+        num1.save()
+        res = Voter(name=fname,respondent_id =r_id,respondent_age =age, spanish_speaking =s_speaking, respondent_wave = r_wave, done_online_survey=done_survey,  phone_no_one =num1)
+## has phone number 2 only
+      if val1 == False & val2 == True & val3 == False:
+        num2.save()
+        res = Voter(name=fname,respondent_id =r_id,respondent_age =age, spanish_speaking =s_speaking, respondent_wave = r_wave, done_online_survey=done_survey,  phone_no_two =num2)
+## has phone number 3 only
+      if val1 == False & val2 == False & val3 == True:
+        num3.save()
+        res = Voter(name=fname,respondent_id =r_id,respondent_age =age, spanish_speaking =s_speaking, respondent_wave = r_wave, done_online_survey=done_survey,  phone_no_three =num3)
+## has phone number 1 and 3 only     
+      if val1 == True & val2 == False & val3 == True:
+        num1.save()
+        num3.save()
+        res = Voter(name=fname,respondent_id =r_id,respondent_age =age, spanish_speaking =s_speaking, respondent_wave = r_wave, done_online_survey=done_survey,  phone_no_one =num1,  phone_no_three =num3)
+## has phone number 1 and 2 only         
+      if val1 == True & val2 == True & val3 == False:
+        num1.save()
+        num2.save()
+        res = Voter(name=fname,respondent_id =r_id,respondent_age =age, spanish_speaking =s_speaking, respondent_wave = r_wave, done_online_survey=done_survey,  phone_no_one =num1,  phone_no_two =num2)
+## has phone number 2 and 3 only 
+      if val1 == False & val2 == True & val3 == True:
+        num2.save()
+        num3.save()
+        res = Voter(name=fname,respondent_id =r_id,respondent_age =age, spanish_speaking =s_speaking, respondent_wave = r_wave, done_online_survey=done_survey,  phone_no_two =num2,  phone_no_three =num3)
+
       res.save()
