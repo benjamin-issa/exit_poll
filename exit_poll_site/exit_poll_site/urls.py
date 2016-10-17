32645b80f18f2fa7a3525040d180b917fe111087
bfissa <benjaminissa@Pineapple.app.byu.edu>
2016-10-17 09:01:29 -0600

    admin page login now redirects

--- exit_poll_site/exit_poll_site/urls.py
+++ exit_poll_site/exit_poll_site/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+
+
+admin.autodiscover()
+admin.site.login = login_required(admin.site.login)
 
 urlpatterns = [
     url(r'^administration/', include('administration.urls')),
