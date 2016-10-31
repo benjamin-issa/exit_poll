70d0453076c99dc303abe4721c74af5123d5366a
bfissa <benjaminissa@Pineapple.local>
2016-10-31 11:32:12 -0600

    v. 1.2.3 -  typo fix... again.

--- exit_poll_site/administration/views.py
+++ exit_poll_site/administration/views.py
@@ -34,10 +34,10 @@ def index(request):
     w2_phone = voters_took_survey_phone.filter(respondent_wave = "W2")
     w2_internet = voters_took_survey_internet.filter(respondent_wave = "W2")
     
-#    w3_voters_took_survey = voters_took_survey.filter(respondent_wave = "W3")
-#    w3_response_rate = (w3_voters_took_survey.count() / float(voters_wavethree.count()) * 100)
-#    w3_phone = voters_took_survey_phone.filter(respondent_wave = "W3")
-#    w3_internet = voters_took_survey_internet.filter(respondent_wave = "W3")
+    w3_voters_took_survey = voters_took_survey.filter(respondent_wave = "W3")
+    w3_response_rate = (w3_voters_took_survey.count() / float(voters_wavethree.count()) * 100)
+    w3_phone = voters_took_survey_phone.filter(respondent_wave = "W3")
+    w3_internet = voters_took_survey_internet.filter(respondent_wave = "W3")
     
 #    w4_voters_took_survey = voters_took_survey.filter(respondent_wave = "W4")
 #    w4_response_rate = (w4_voters_took_survey.count() / float(voters_wavefour.count()) * 100)
@@ -54,8 +54,8 @@ def index(request):
                                           'w1_phone': w1_phone, 'w1_internet': w1_internet,
                                           'w2_voters_took_survey': w2_voters_took_survey, 'w2_response_rate': w2_response_rate,
                                           'w2_phone': w2_phone, 'w2_internet': w2_internet,
-#                                          'w3_voters_took_survey': w3_voters_took_survey, 'w3_response_rate': w3_response_rate,
-#                                          'w3_phone': w3_phone, 'w3_internet': w3_internet,
+                                          'w3_voters_took_survey': w3_voters_took_survey, 'w3_response_rate': w3_response_rate,
+                                          'w3_phone': w3_phone, 'w3_internet': w3_internet,
 #                                          'w4_voters_took_survey': w4_voters_took_survey, 'w4_response_rate': w4_response_rate,
 #                                          'w4_phone': w4_phone, 'w4_internet': w4_internet,
                                           })
